0e181272c89b63d614e28262ad72aa14d22e794f
kentakom1213 <kentakom1213@gmail.com>
2022-10-27 20:17:45 +0900

    [add] プロット

--- No01-perceptron/sigmoid.rs
+++ No01-perceptron/sigmoid.rs
@@ -1,2 +1,11 @@
 
+fn sigmoid(x: f64) -> f64 {
+    1.0 / (1.0 + x.exp())
+}
 
+fn main() {
+    for i in -50..50 {
+        let x = (i as f64) / 10.0;
+        println!("{:3.3} : {}", x, sigmoid(-x));
+    }
+}
